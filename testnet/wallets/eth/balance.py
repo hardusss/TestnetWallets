fab0f36b7d57fb7d2d473d88d07359c539d0dbcb
hardusss <danylo29bro@gmail.com>
2025-10-23 09:08:27 +0300

    refactor: add typing and try/except, and convert balance from wei

--- testnet/wallets/eth/balance.py
+++ testnet/wallets/eth/balance.py
@@ -1,22 +1,42 @@
 from testnet.interfaces.logs import Logger
 from testnet.interfaces import AddressBalanceInterface
 from typing import Optional
+from decimal import Decimal
 from web3 import Web3, HTTPProvider
+from web3.types import Wei
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-class SolAddressBalance(AddressBalanceInterface):
+class EthAddressBalance(AddressBalanceInterface):
     @staticmethod
-    def get_balance(wallet_name: Optional[str] = None, address: Optional[str] = None) -> float:
+    def get_balance(wallet_name: Optional[str] = None, address: Optional[str] = None) -> float | Decimal:
+        logger = Logger("eth-addresses-balance.log").get_logger()
         try:
-            w3 = Web3(HTTPProvider(os.getenv("ETH_RPC_LINK")))
-            if w3.is_connected():
-                balance = w3.eth.get_balance(address)
+            rpc_url: str | None = os.getenv("ETH_RPC_LINK")
+            if not rpc_url:
+                logger.error("Failed to get rpc url from .env file")
+                return 0.0
             
-            else: return 0.0
+            w3 = Web3(HTTPProvider(rpc_url))
+            if not w3.is_connected():
+                logger.error("Failed to connect to provider RPC")
+                return 0.0
 
+            if not address or not Web3.is_address(address):
+                logger.error(f"Address {address} is not valid")
+                return 0.0
+
+            balance_wei: Wei = w3.eth.get_balance(address)
+            balance: Decimal = w3.from_wei(number=balance_wei, unit="ether")
+            logger.info(f"Successfully got balance for address: {address}, balance: {balance}")
+            return balance
+            
         except Exception as ex:
+            logger.error(f"Unknown error while getting balance for address {address}: {ex}")
             return 0.0
         
+if __name__ == "__main__":
+    print(EthAddressBalance.get_balance(address="0x0feC4Ada3De77C2D9B92a1af906f1b50F994cf9A"))       
